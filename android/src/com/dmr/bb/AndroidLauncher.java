866fede108b189f9faa7a132908c8b9ada0efc1b
Adam01010101F <watsaname1999@gmail.com>
2018-11-27 16:33:44 -0800

    git init

---
+++ android/src/com/dmr/bb/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dmr.bb;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dmr.bb.BBGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BBGame(), config);
+	}
+}
